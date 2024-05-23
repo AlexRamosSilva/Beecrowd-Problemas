b399fee34cd4147728d9fc2f9eac1e887fa1d08d
AlexRamosSilva <164268004+AlexRamosSilva@users.noreply.github.com>
2024-05-23 13:48:32 -0300

    Update Beecrowd 1018 Cédulas.c

--- Beecrowd 1018 Cédulas.c
+++ Beecrowd 1018 Cédulas.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    int tempo, velocidade, distancia;
-    double litros;
+    int N;
 
-    scanf("%d\n%d", &tempo, &velocidade);
+    scanf("%d", &N);
 
-    distancia = velocidade * tempo;
-    litros = distancia/12.0;
-
-    printf("%.3lf\n", litros);
+    printf("%d\n", N);
+    
+    printf("%d nota(s) de R$ 100,00\n", N/100);
+    N %= 100;
+    printf("%d nota(s) de R$ 50,00\n", N/50);
+    N %= 50;
+    printf("%d nota(s) de R$ 20,00\n", N/20);
+    N %= 20;
+    printf("%d nota(s) de R$ 10,00\n", N/10);
+    N %= 10;
+    printf("%d nota(s) de R$ 5,00\n", N/5);
+    N %= 5;
+    printf("%d nota(s) de R$ 2,00\n", N/2);
+    N %= 2;
+    printf("%d nota(s) de R$ 1,00\n", N);
 
     return 0;
 }
