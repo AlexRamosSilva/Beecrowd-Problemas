68e35fe108ffd82759a153af015a6844f9ff515d
AlexRamosSilva <164268004+AlexRamosSilva@users.noreply.github.com>
2024-05-23 13:43:57 -0300

    Update Beecrowd 1012 Área.c

--- Beecrowd 1012 Área.c
+++ Beecrowd 1012 Área.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int maior(int a, int b){
-    return (a + b + abs(a - b))/2;
-}
 
 int main(){
-    int a, b, c, resposta;
+    double A, B, C;
+    const double PI = 3.14159;
 
-    scanf("%d %d %d", &a, &b, &c);
-    
-    resposta = maior(a, maior(b, c));
+    scanf("%lf\n%lf\n%lf", &A, &B, &C);
 
-    printf("%d eh o maior\n", resposta);
+    printf("TRIANGULO: %.3lf\n", (A * C)/2.0);
+    printf("CIRCULO: %.3lf\n", PI * C * C);
+    printf("TRAPEZIO: %.3lf\n", (A + B)/2.0 * C);
+    printf("QUADRADO: %.3lf\n", B * B);
+    printf("RETANGULO: %.3lf\n", A * B);
 
     return 0;
 }
