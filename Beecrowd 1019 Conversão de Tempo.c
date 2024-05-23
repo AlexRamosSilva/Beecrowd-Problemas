ab35a99ac1b84d845fe7cd7c2d174686c3775718
AlexRamosSilva <164268004+AlexRamosSilva@users.noreply.github.com>
2024-05-23 13:49:13 -0300

    Update Beecrowd 1019 Conversão de Tempo.c

--- Beecrowd 1019 Conversão de Tempo.c
+++ Beecrowd 1019 Conversão de Tempo.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int N;
+    int segundos, horas, minutos;
 
-    scanf("%d", &N);
+    scanf("%d", &segundos);
 
-    printf("%d\n", N);
-    
-    printf("%d nota(s) de R$ 100,00\n", N/100);
-    N %= 100;
-    printf("%d nota(s) de R$ 50,00\n", N/50);
-    N %= 50;
-    printf("%d nota(s) de R$ 20,00\n", N/20);
-    N %= 20;
-    printf("%d nota(s) de R$ 10,00\n", N/10);
-    N %= 10;
-    printf("%d nota(s) de R$ 5,00\n", N/5);
-    N %= 5;
-    printf("%d nota(s) de R$ 2,00\n", N/2);
-    N %= 2;
-    printf("%d nota(s) de R$ 1,00\n", N);
+    horas = segundos/3600;
+    segundos %= 3600;
+    minutos = segundos/60;
+    segundos %= 60;
+
+    printf("%d:%d:%d\n", horas, minutos, segundos);
 
     return 0;
 }
