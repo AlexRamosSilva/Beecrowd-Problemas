04a4e0968f5d2392381cb118cbe2d218d71fe55c
AlexRamosSilva <164268004+AlexRamosSilva@users.noreply.github.com>
2024-05-23 13:46:16 -0300

    Update Beecrowd 1015 Distância Entre Dois Pontos.c

--- Beecrowd 1015 Distância Entre Dois Pontos.c
+++ Beecrowd 1015 Distância Entre Dois Pontos.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-    int X;
-    double Y, consumo;
+    double x1, y1, x2, y2, distancia;
 
-    scanf("%d\n%lf", &X, &Y);
-    
-    consumo = X/Y;
+    scanf("%lf %lf\n%lf %lf", &x1, &y1, &x2, &y2);
 
-    printf("%.3lf km/l\n", consumo);
+    distancia = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+    printf("%.4lf\n", distancia);
 
     return 0;
 }
